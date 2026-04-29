746e25df2b6d5a1efbd65ae409200a137ead3bc6
AlbertoooMP <159181260+AlbertoooMP@users.noreply.github.com>
2026-04-29 23:12:50 +0200

    Solución acceso a perfil de usuario, implementada vista para solicitar verificación de email, además se ha implementado la lógica para verificar el email

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -45,5 +45,6 @@ class FortifyServiceProvider extends ServiceProvider
 
         Fortify::loginView(fn () => view('auth.login'));
         Fortify::registerView(fn () => view('auth.signin'));
+        Fortify::verifyEmailView(fn () => view('auth.verify-email'));
     }
 }
