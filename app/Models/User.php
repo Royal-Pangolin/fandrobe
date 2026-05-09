f3931ed3acb69a66fb915dd3c62c84c9ed0a609f
AlbertoooMP <159181260+AlbertoooMP@users.noreply.github.com>
2026-05-09 19:47:58 +0200

    Revert "Operaciones CRUD para direcciones añadidas al perfil del usuario"

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,14 +72,6 @@ class User extends Authenticatable implements MustVerifyEmail
             ->withPivot('followed_at');
     }
 
-    /**
-     * Direcciones de envío del usuario.
-     */
-    public function addresses(): HasMany
-    {
-        return $this->hasMany(Address::class);
-    }
-
     public function hasVerifiedEmail(): bool
     {
         return (bool) $this->email_verified;
