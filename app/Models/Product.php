7678f82d42e00546d61e8c8db15ad0227d208c72
AlbertoooMP <159181260+AlbertoooMP@users.noreply.github.com>
2026-05-10 18:56:15 +0200

    Añadido lista de productos más vendidos en panel de administración, e implementada lógica productos más vendidos vista home

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -61,6 +61,11 @@ class Product extends Model
         return $this->hasMany(Review::class);
     }
 
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
     public function favorites()
     {
         return $this->hasMany(Favorite::class);
